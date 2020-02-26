4264f4f51968b3125d6af4ed3231e4ecfe13ab63
Mathias Rasmussen <mathiasvr@gmail.com>
2020-02-27 00:30:05 +0100

    select and display multiple profiles

--- src/renderer/VariablesView.svelte
+++ src/renderer/VariablesView.svelte
@@ -1,13 +1,20 @@
 <script>
   import VariableTable from './components/VariableTable.svelte'
 
+  // todo: get active prof and rateprof
   export let bfvariables
+
+  // todo: get from bf
+  const profiles = [1, 2, 3]
+  const rateprofiles = [1, 2, 3, 4, 5, 6]
+  
+  let selectedProfile = profiles[0]
+  let selectedRateProfile = rateprofiles[0]
 
   // todo: update on change in app?
   const masterVariables = Object.entries(bfvariables.masterVars)
-  // TODO: allow switching (currently just profile/rate 0)
-  const profileVariables = Object.entries(bfvariables.profilesVars[0])
-  const rateVariables = Object.entries(bfvariables.rateProfilesVars[0])
+  $: profileVariables = Object.entries(bfvariables.profilesVars[selectedProfile - 1])
+  $: rateVariables = Object.entries(bfvariables.rateProfilesVars[selectedRateProfile - 1])
 </script>
 
 <div>
@@ -17,8 +24,24 @@
   <VariableTable variables={masterVariables} />
 
   <h2>Profile Variables</h2>
+  <form class="form-inline my-4">
+    <label class="mr-sm-2" for="profileSelect">Profile</label>
+    <select class="custom-select" id="profileSelect" bind:value={selectedProfile}>
+      {#each profiles as p}
+      <option value={p}>{p}</option>
+      {/each}
+    </select>
+  </form>
   <VariableTable variables={profileVariables} />
 
   <h2>Rate Profile Variables</h2>
+  <form class="form-inline my-4">
+    <label class="mr-sm-2" for="rateSelect">Rate profile</label>
+    <select class="custom-select" id="rateSelect" bind:value={selectedRateProfile}>
+      {#each rateprofiles as r}
+      <option value={r}>{r}</option>
+      {/each}
+    </select>
+  </form>
   <VariableTable variables={rateVariables} />
 </div>
