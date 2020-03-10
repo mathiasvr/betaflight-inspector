c525f53679a5c1512a1ff92251fbcdb12f185eb8
Mathias Rasmussen <mathiasvr@gmail.com>
2020-03-10 19:50:41 +0100

    clean up VariableRow

--- src/renderer/components/VariableRow.svelte
+++ src/renderer/components/VariableRow.svelte
@@ -1,16 +1,13 @@
 <script>
   import { slide } from 'svelte/transition'
   import marked from 'marked'
-  // import SvgInfo from 'bootstrap-icons/icons/question-square-fill.svg'
   import SvgInfo from 'bootstrap-icons/icons/info-square.svg'
   import SvgUndo from 'bootstrap-icons/icons/arrow-counterclockwise.svg'
   import OSDPosition from './osdPosition.svelte'
 
   export let variable
-  // copy of variable (bound to user changes)
+  // TODO: rename?
   $: data = variable
-  // TODO: key may not be needed for id's
-  $: key = data.name
 
   let showHelp = false
 </script>
@@ -28,14 +25,14 @@
       <div class="form-row align-items-center">
         {#if Math.abs(data.range[1] - data.range[0]) <= 10000}
         <div class="col">
-          <input class="custom-range" id="{key}__opt" type="range" min={data.range[0]} max={data.range[1]} bind:value={data.value}/>
+          <input class="custom-range" type="range" min={data.range[0]} max={data.range[1]} bind:value={data.value}/>
         </div>
         {/if}
         
         <div class="col-sm-auto">
           <!-- TODO: should this only be -sm when there is range slider? -->
           <div class="input-group input-group-sm">
-            <input class="form-control" type="number" id="{key}__displ2" min={data.range[0]} max={data.range[1]} bind:value={data.value}/>
+            <input class="form-control" type="number" min={data.range[0]} max={data.range[1]} bind:value={data.value}/>
             {#if data.unit}
             <div class="input-group-append">
               <span class="input-group-text">{data.unit}</span>
@@ -55,12 +52,11 @@
       <!-- TODO: make this type boolean before-hand for easier binding? -->
       {#if data.allowed.length === 2 && data.allowed.includes('ON') && data.allowed.includes('OFF')}
       <div class="custom-control custom-switch">
-        <input class="custom-control-input" id="{key}__opt" type="checkbox" checked={data.value === 'ON'} on:change={data.value = this.checked ? 'ON' : 'OFF' } />
-        <!-- TODO: why is label needed? -->
-        <label class="custom-control-label" for="{key}__opt"></label>
+        <input class="custom-control-input" id="{data.name}_switch" type="checkbox" checked={data.value === 'ON'} on:change={data.value = this.checked ? 'ON' : 'OFF' } />
+        <label class="custom-control-label" for="{data.name}_switch"></label>
       </div>
       {:else}
-      <select class="custom-select custom-select-sm" id="{key}__opt" bind:value={data.value}>
+      <select class="custom-select custom-select-sm" bind:value={data.value}>
         {#each data.allowed as option}
         <option value={option}>{option}</option>
         {/each}
@@ -69,11 +65,10 @@
     {:else}
     <!-- TODO: clean up '-' values to empty string -->
     <!-- TODO: add placeholder="RATE_1", etc.-->
-    <input class="form-control" id="{key}__opt" type="text" bind:value={data.value} />
+    <input class="form-control" type="text" bind:value={data.value} />
     {/if}
   </div>
 
-  <!-- TODO: fix buttons, don't use "close" labels, etc. -->
   <div class="col my-1" style="color:#0062cc;">
     {#if data.value.toString() !== data.default.toString()}
     <a style="font-size:2em;line-height:0" href="#reset" title="Reset to default" on:click={e => (data.value = data.default)}>
@@ -90,9 +85,6 @@
       {@html SvgInfo}
     </a>
     {/if}
-    <!-- <button type="button" class="" aria-label="Close" title="Reset to default value"><i class="fas fa-undo-alt" aria-hidden="true"></i></button>
-    <button type="button" class="" aria-label="Close" title="Report issue"><i class="fas fa-exclamation-circle" aria-hidden="true"></i></button>
-    <button type="button" class="" aria-label="Close" title="Connect"><i class="fas fa-exchange-alt" aria-hidden="true"></i></button> -->
   </div>
 
   {#if data.desc && showHelp}
